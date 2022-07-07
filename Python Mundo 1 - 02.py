f13bada67cce3e8200151ebfc7711ff6fc773b09
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-07 17:07:36 -0300

    Aula 06 aos 20:50

---
+++ Python Mundo 1 - 02.py
@@ -0,0 +1,4 @@
+termo1=input('Digite um termo')
+print('O termo digitado foi {} e é uma string, pois eu não especifiquei o tipo primitivo'.format(termo1))
+termo2=int(input('Digite um outro termo numérico inteiro'))
+print('Esse outro termo, {}, é um número inteiro pois eu especifiquei o seu tipo'.format(termo2))
