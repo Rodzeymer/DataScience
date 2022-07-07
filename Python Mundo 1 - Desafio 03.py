7496ab2bb30ef602b3a31847862e1237307d3524
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-07 16:55:02 -0300

    Código corrigido

---
+++ Python Mundo 1 - Desafio 03.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Olá, digite o primeiro termo.'))
+print('Primeiro termo digitado: {}'.format(numero1))
+numero2 = int(input('Agora digite o segundo termo'))
+print('Segundo termo digitado: {}'.format(numero2))
+soma = numero1 + numero2
+print('Os termos digitados foram: {} e {}'.format(numero1, numero2))
+print('A soma deles é: {}'.format(soma))
