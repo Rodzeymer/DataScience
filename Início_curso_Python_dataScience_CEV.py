b2e2ae91a8c9f852578eb8aa8feb72ae0f36e99a
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-07 10:57:24 -0300

    Create Início_curso_Python_dataScience_CEV.py

---
+++ Início_curso_Python_dataScience_CEV.py
@@ -0,0 +1,8 @@
+nome = input('Qual o seu nome?')
+
+idade = input('Qual a sua idade?')
+
+peso = input('Quanto você pesa?')
+
+print('O funcionário ',nome, 'tem ', idade, ' anos de idade e pesa '
+, peso, ' quilos.')
