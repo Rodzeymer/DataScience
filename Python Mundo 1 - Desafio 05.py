2fc639fd7096f40870c649dcce55a541951ef1d2
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-11 16:48:21 -0300

    Resolvido

---
+++ Python Mundo 1 - Desafio 05.py
@@ -0,0 +1,8 @@
+#Desafio dos sucessores e antecessores
+
+valor = int(input('Digite o número a ser analisado'))
+
+antes = valor - 1
+depois = valor + 1
+
+print('O sucessor de {} é {} \n e o antecessor de {} é {}'.format(valor, depois, valor, antes))
