c81df1f023ff293b1125941603699b8b8994ed19
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-11 11:43:53 -0300

    Desafio 04

---
+++ Python Mundo 1 - Desafio 04.py
@@ -0,0 +1,11 @@
+digitado = input('Digite algo!')
+print('Foi digitado {}.'.format(digitado))
+
+if digitado.isalnum():
+    print('O {} são letras ou números'.format(digitado))
+else:
+    print('O {} não são letras nem números'.format(digitado))   
+if digitado.isalpha():
+    print('O {} são letras'.format(digitado))
+if digitado.isnumeric():
+    print('O {} são números'.format(digitado))
