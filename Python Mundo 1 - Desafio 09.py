ffc6518399a9cbda8d3bc5fa186cd05399c339c1
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-11 20:25:02 -0300

    Pronto

---
+++ Python Mundo 1 - Desafio 09.py
@@ -0,0 +1,14 @@
+#Desafio tabuada primitiva
+
+numero = float(input('Digite o valor da tabuada'))
+
+print('{} * 1 = {:.2f}'.format(numero, numero*1))
+print('{} * 2 = {:.2f}'.format(numero, numero*2))
+print('{} * 3 = {:.2f}'.format(numero, numero*3))
+print('{} * 4 = {:.2f}'.format(numero, numero*4))
+print('{} * 5 = {:.2f}'.format(numero, numero*5))
+print('{} * 6 = {:.2f}'.format(numero, numero*6))
+print('{} * 7 = {:.2f}'.format(numero, numero*7))
+print('{} * 8 = {:.2f}'.format(numero, numero*8))
+print('{} * 9 = {:.2f}'.format(numero, numero*9))
+print('{} * 10 = {:.2f}'.format(numero, numero*10))
