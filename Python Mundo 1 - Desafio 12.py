bb608cd1bf2028968c38b943c987583f3b6df21c
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-12 10:02:17 -0300

    1

--- Python Mundo 1 - Desafio 12.py
+++ Python Mundo 1 - Desafio 12.py
@@ -15,4 +15,4 @@ reajuste = float(input('Agora digite o percentual de reajuste'))
 
 reajusteP = reajuste/100
 
-print('O produto que valia R${}, com o reajuste de {:.2f}%, etá com o preço de R${}'.format(precoOriginal, reajusteP, precoOriginal*(reajusteP+1)))
+print('O produto que valia R${}, com o reajuste de {:.2f}%, etá com o preço de R${}'.format(precoOriginal, reajusteP, precoOriginal*(reajusteP-1)))
