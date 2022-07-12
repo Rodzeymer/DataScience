315def67f8cd953b8ee958de3be3744499847d00
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-12 09:38:28 -0300

    Melhorado

--- Python Mundo 1 - Desafio 10.py
+++ Python Mundo 1 - Desafio 10.py
@@ -1,8 +1,19 @@
 #desafio dinheiro na carteira
 
 realCarteira = float(input('Quanto dinheiro em real você tem na carteira?'))
+moeda = input('Para qual moeda deseja converter? \n D de Dólar? \n E de Euro? \n Y de Yene?')
 
-cotaçãoRealDolar = 0.185932357
-comprarDolar = realCarteira * cotaçãoRealDolar
+if moeda.capitalize() == 'D':
+    cotacaoMoeda = 5.38
+    comprarMoeda = realCarteira / cotacaoMoeda
+    print('Com R${} você consegue comprar hoje US${:.2f}'.format(realCarteira, comprarMoeda))
+    
+if moeda.capitalize() == 'E':
+    cotacaoMoeda = 5.41
+    comprarMoeda = realCarteira / cotacaoMoeda
+    print('Com R${} você consegue comprar hoje EU${:.2f}'.format(realCarteira, comprarMoeda))
 
-print('Com R${} você consegue comprar hoje US${:.2f}'.format(realCarteira, comprarDolar))
+if moeda.capitalize() == 'Y':
+    cotacaoMoeda = 0.039
+    comprarMoeda = realCarteira / cotacaoMoeda
+    print('Com R${} você consegue comprar hoje Y${:.2f}'.format(realCarteira, comprarMoeda))
