4625e9ee0fde7c3c06a8dd0d0acda79b9c1fe2ab
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-12 09:29:34 -0300

    remodelado

--- Python Mundo 1 - Desafio 09.py
+++ Python Mundo 1 - Desafio 09.py
@@ -1,15 +1,15 @@
 #Desafio tabuada primitiva
 
-numero = float(input('Digite o valor da tabuada'))
+numero = int(input('Digite o valor da tabuada'))
 
 
-print('{} * 1 = {:.2f}'.format(numero, numero*1))
-print('{} * 2 = {:.2f}'.format(numero, numero*2))
-print('{} * 3 = {:.2f}'.format(numero, numero*3))
-print('{} * 4 = {:.2f}'.format(numero, numero*4))
-print('{} * 5 = {:.2f}'.format(numero, numero*5))
-print('{} * 6 = {:.2f}'.format(numero, numero*6))
-print('{} * 7 = {:.2f}'.format(numero, numero*7))
-print('{} * 8 = {:.2f}'.format(numero, numero*8))
-print('{} * 9 = {:.2f}'.format(numero, numero*9))
-print('{} * 10 = {:.2f}'.format(numero, numero*10))
+print('{} * {:2} = {:2}'.format(numero, 1, numero*1))
+print('{} * {:2} = {:2}'.format(numero, 2, numero*2))
+print('{} * {:2} = {:2}'.format(numero, 3, numero*3))
+print('{} * {:2} = {:2}'.format(numero, 4, numero*4))
+print('{} * {:2} = {:2}'.format(numero, 5, numero*5))
+print('{} * {:2} = {:2}'.format(numero, 6, numero*6))
+print('{} * {:2} = {:2}'.format(numero, 7, numero*7))
+print('{} * {:2} = {:2}'.format(numero, 8, numero*8))
+print('{} * {:2} = {:2}'.format(numero, 9, numero*9))
+print('{} * {:2} = {:2}'.format(numero, 10, numero*10))
