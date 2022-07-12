4b155258a81666ed81c8581174720642adae6765
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-07-12 09:14:20 -0300

    Refatorado

--- Python Mundo 1 - Desafio 06.py
+++ Python Mundo 1 - Desafio 06.py
@@ -1,17 +1,11 @@
 #Desafio modificadores
-import math
+
 valor = int(input('Digite um valor a ser analisado'))
 
-dobro = valor*2
-triplo = valor*3
-quadrado = valor**2
-cubo = valor**3
-raiz2 = valor**0.5
-raiz3 = valor**0.3333
-
-print('O dobro de {} é {}'.format(valor, dobro))
-print('O triplo de {} é {}'.format(valor, triplo))
-print('{} elevado ao quadrado é {}'.format(valor, quadrado))
-print('{} elevado ao cubo é {}'.format(valor, cubo))
-print('A raiz quadrada de {} é {}'.format(valor, raiz2))
-print('A raiz cúbica de {} é {}'.format(valor, raiz3))
+print('O dobro de {} é {}'.format(valor, valor*2))
+print('O triplo de {} é {}'.format(valor, valor*3))
+print('{} elevado ao quadrado é {}'.format(valor, valor**2))
+print('{} elevado ao cubo é {}'.format(valor, valor**3))
+print('A raiz quadrada de {} é {}'.format(valor, valor**(1/2)))
+print('A raiz cúbica de {} é {}'.format(valor, valor**(1/3)))
+print('A raiz quarta de {} é {}'.format(valor, valor**(1/4)))
