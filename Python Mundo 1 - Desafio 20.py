379726d01188b4874e87541839f5a9b0495f0660
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-12 15:40:27 -0300

    20 corrigido

--- Python Mundo 1 - Desafio 20.py
+++ Python Mundo 1 - Desafio 20.py
@@ -1,8 +1,8 @@
-from random import sample
+import random
 
 confirmar = input('Aperte enter para sortear a ordem dentre os alunos abaixo: \n Arthur, Bruno, Claudio, Daniel.')
 
 alunos = ['Arthur', 'Bruno', 'Claudio', 'Daniel']
-sequenciaAlunos = sample(alunos, len(alunos))
+random.shuffle(alunos)
 
-print('A sequencia selecionada foi: {}'.format(sequenciaAlunos))
+print('A sequencia selecionada foi: {}'.format(alunos))
