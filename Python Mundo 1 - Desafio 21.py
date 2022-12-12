9098e8cd5a6a2d48196193a403823ba6ee2fac57
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-12 16:53:47 -0300

    por hoje

--- Python Mundo 1 - Desafio 21.py
+++ Python Mundo 1 - Desafio 21.py
@@ -1,3 +1,5 @@
 import playsound
 
 playsound('https://www.youtube.com/watch?v=Jly9qp40rfw') 
+
+https://www.cursoemvideo.com/curso/python-3-mundo-1/aulas/usando-modulos-do-python/modulos/exercicio-21-tocando-um-mp3/
