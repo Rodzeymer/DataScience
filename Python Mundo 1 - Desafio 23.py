974ac93eef72b732c21bef0eb318a916a34a854f
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-13 20:35:20 -0300

    Desafio 23 DONE

--- Python Mundo 1 - Desafio 23.py
+++ Python Mundo 1 - Desafio 23.py
@@ -1,13 +1,28 @@
-numero = int(input('Digite um número de 1 a 9999'))
+numero = int(input("Digite um numero de 1 a 9999"))
 
+if numero <10:
+	print('0 milhar, 0 centena, 0 dezena, {} unidades'.format(numero))
+	
+elif numero >= 10 and numero <100:
+	numeroQuebrado=[int(d) for d in str(numero)]
+	numeroDezena=numeroQuebrado[0]
+	numeroUnidade=numeroQuebrado[1]
+	print('0 milhar, 0 centena, {} dezenas, {} unidades'.format(numeroDezena, numeroUnidade))
 
-if numero < 10:
-    print("Unidades: ", numero)
-
-elif numero >= 10 and numero < 100:
-    numeroStr = str(numero)
-
-
-https://www.cursoemvideo.com/curso/python-3-mundo-1/aulas/usando-modulos-do-python/modulos/manipulando-texto/
-
-42.26
+elif numero >= 100 and numero <1000:
+	numeroQuebrado=[int(d) for d in str(numero)]
+	numeroCentena=numeroQuebrado[0]
+	numeroDezena=numeroQuebrado[1]
+	numeroUnidade=numeroQuebrado[2]
+	print('0 milhar, {} centena, {} dezenas, {} unidades'.format(numeroCentena, numeroDezena, numeroUnidade))
+	
+elif numero >= 1000 and numero <10000:
+	numeroQuebrado=[int(d) for d in str(numero)]
+	numeroMilhar=numeroQuebrado[0]
+	numeroCentena=numeroQuebrado[1]
+	numeroDezena=numeroQuebrado[2]
+	numeroUnidade=numeroQuebrado[3]
+	print('{} milhar, {} centena, {} dezenas, {} unidades'.format(numeroMilhar, numeroCentena, numeroDezena, numeroUnidade))
+	
+elif numero >= 10000:
+	print('O numero {} e muito grande!'.format(numero))
