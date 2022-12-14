070a7892d89cadb7a2218822a6bdb1ceb7f0ee92
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-14 16:53:58 -0300

    por hoje é só

--- Python Mundo 1 - Desafio 30.py
+++ Python Mundo 1 - Desafio 30.py
@@ -4,3 +4,7 @@ if numero%2 == 0:
     print('O número {} é par.'.format(numero))
 else:
     print('O número {} é impar.'.format(numero))
+
+    https://www.cursoemvideo.com/curso/python-3-mundo-1/aulas/condicoes-em-python-if-else/modulos/condicoes-parte-1/
+
+    29:26
