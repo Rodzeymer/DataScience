fbaea374ea7474540406d1172465dd778e9b56d5
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-14 16:37:10 -0300

    28 finalizado

---
+++ Python Mundo 1 - Desafio 28.py
@@ -0,0 +1,11 @@
+import random
+
+numeroEscolhido = random.randint(0, 5)
+print(numeroEscolhido)
+
+palpite = int(input('Tente adivinha o número escolhido, de 0 a 5'))
+
+if palpite == numeroEscolhido:
+    print('ACERTOU! Seu palpite foi {} o mesmo que o número escolhido {}'.format(palpite, numeroEscolhido))
+else:
+    print('Errou, o número escolhido foi {} e você digitou {}'.format(numeroEscolhido, palpite))
