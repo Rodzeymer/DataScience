45c184f2c901efdc7e30849106d28ec61082064b
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-14 15:10:31 -0300

    27 feito

---
+++ Python Mundo 1 - Desafio 27.py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome completo')
+
+nomeSeparado = nome.split()
+
+primeiroNome = nomeSeparado[0]
+ultimoNome = nomeSeparado[len(nomeSeparado)-1]
+
+print(nome)
+print(primeiroNome)
+print(ultimoNome)
