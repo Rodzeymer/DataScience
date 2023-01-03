82b2d65b8c8f8ea64c036d44c2530c2e9129e48c
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-03 10:43:01 -0300

    Desafio 03 formatado, melhorado e comentado

--- Python Mundo 1 - Desafio 03.py
+++ Python Mundo 1 - Desafio 03.py
@@ -1,3 +1,10 @@
+# Neste desafio o usuário irá dar entrada em 2 números e o programa retornará soma entre eles, 
+# um desafio bem simples para aquecer os motores, de agora em diante vamos com tudo!
+
+print('*' *30)
+print(f"{'Python Mundo 1 - Desafio 03':^30}")
+print('*' *30)
+
 numero1 = int(input('Olá, digite o primeiro termo.'))
 print('Primeiro termo digitado: {}'.format(numero1))
 numero2 = int(input('Agora digite o segundo termo'))
@@ -5,3 +12,8 @@ print('Segundo termo digitado: {}'.format(numero2))
 soma = numero1 + numero2
 print('Os termos digitados foram: {} e {}'.format(numero1, numero2))
 print('A soma deles é: {}'.format(soma))
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
+    
