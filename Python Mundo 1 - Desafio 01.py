1caf39dfb59d8cf8c4a792b76f32bb4d5fab4eb3
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-03 10:13:48 -0300

    Desafio 01 formatado, melhorado e comentado

--- Python Mundo 1 - Desafio 01.py
+++ Python Mundo 1 - Desafio 01.py
@@ -1,2 +1,10 @@
+# Começando pelo começo, esse script vai perguntar o nome do usuário 
+# e imprimir a mensagem de boas vindas com o nome digitado!
+
+print('*' *30)
+print(f"{'Python Mundo 1 - Desafio 01':^30}")
+print('*' *30)
+
 name = input('Olá, seja bem vindo, qual o seu nome?')
-print('Seja muito bem vindo {} como posso te ajudar?'.format(name))
+print('Seja muito bem vindo {}, como posso te ajudar?'.format(name))
+print('*' *30)
