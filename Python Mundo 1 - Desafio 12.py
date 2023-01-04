73f6a731939bace0dda5b24b2eb9d4cf63f396f7
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-03 23:02:57 -0300

    Desafio 12

--- Python Mundo 1 - Desafio 12.py
+++ Python Mundo 1 - Desafio 12.py
@@ -1,9 +1,18 @@
-#Desafio desconto produto
+# Este desafio foi construir um algoritmo que calcula o valor final de um produto, o valor original
+# do produto e seu reajuste deve ser informado pelo usuário e o programa devolve o valor inicial
+# o percentual de reajuste e o valor reajustado.
+
+print('*' *30)
+print(f"{'Python Mundo 1 - Desafio 12':^30}")
+print('*' *30)
 
 precoOriginal = float(input('Digite o valor original do produto'))
-
 reajuste = float(input('Agora digite o percentual de reajuste'))
 
-reajusteP = reajuste/100
+precodesconto = precoOriginal-(precoOriginal*reajuste/100)
 
-print('O produto que valia R${}, com o reajuste de {:.2f}%, etá com o preço de R${}'.format(precoOriginal, reajusteP, precoOriginal*(reajusteP-1)))
+print('O produto que valia R${}, com o reajuste de {:.2f}%, está com o preço de R${}'.format(precoOriginal, reajuste, precodesconto))
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
